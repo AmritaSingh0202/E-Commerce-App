d293d5df816f39db3470f5ff9394009e3ddf7d33
Amrita Singh <147167787+AmritaSingh0202@users.noreply.github.com>
2025-12-16 21:18:05 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "AZQPH1xhXCOctT2Vka5PrVzFQTC2k53ZAxexAlN0WUtETl2CLW05UE4714aGrA-C4MihwBj5hlsPDWBZ",
-  client_secret: "EH8qXdIswgqApc_dzQwXmG7QDKbzb5Y3audg-B1U3JikzgJvwbUh4JdYSEXYV1lYuNvkQSF83G9CSiLz",
+  client_id: "",
+  client_secret: "",
 });
 
 module.exports = paypal;
